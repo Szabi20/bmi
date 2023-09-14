2baae3a59a84f8aa197624a853dcbbc3b4eca44c
szt2GémesSzabolcs <115459925+Szabi20@users.noreply.github.com>
2023-09-14 13:08:58 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Hány kilo: ");
+        double weight = sc.nextDouble();
+        System.out.print(" Hány méter: ");
+        double height = sc.nextDouble();
+        double bmi = weight / (height * height);
+        System.out.print(" A testömeg index (BMI) " + bmi + " kg/m2");
+
+    }
+}
